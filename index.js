94077fbe56ef0ae2b85ba9a8f4db503c2027d430
Lyall <me@lyall.lol>
2024-09-05 10:47:18 +0100

    timestamps

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let lastIp;
         const ip = config.ipKey.split(".").reduce((curr, prev) => curr[prev], ipJson);
         if (!ip || lastIp === ip) return;
         
-        console.log(`IP changed from '${lastIp || "nothing"}' to '${ip || "nothing"}'`);
+        console.log(timestamp(), `IP changed from '${lastIp || "nothing"}' to '${ip || "nothing"}'`);
 
         await fetch(`https://api.cloudflare.com/client/v4/zones/${config.zoneId}/dns_records/${config.dnsRecordId}`, {
             method: "PATCH",
@@ -22,13 +22,17 @@ let lastIp;
         }).then(async res => {
             if (res.status === 200) {
                 lastIp = ip;
-                return console.log("Succesfully updated DNS!");
+                return console.log(timestamp(), "Succesfully updated DNS!");
             }
 
             const json = await res.json().catch(err => { });
-            return console.log(`Failed to update DNS! Status: ${res.status}, JSON:`, json);
-        }).catch(err => console.log("Failed to update DNS! Error:", err));
-    }).catch(err => console.log("Failed to get IP! Error:", err));
+            return console.log(timestamp(), `Failed to update DNS! Status: ${res.status}, JSON:`, json);
+        }).catch(err => console.log(timestamp(), "Failed to update DNS! Error:", err));
+    }).catch(err => console.log(timestamp(), "Failed to get IP! Error:", err));
 
     setTimeout(() => check(), config.checkInterval);
 })();
+
+function timestamp() {
+    return `[${new Date().toLocaleString()}]`;
+}
