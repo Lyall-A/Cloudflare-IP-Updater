f6eae5050d4cd39c3185e5be72a53f4e86621359
Lyall <me@lyall.lol>
2024-09-18 19:23:42 +0100

    aaaaaa

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const config = require("./config.json");
 let lastIp;
 
 (async function check() {
+    // Get IP
     await fetch(config.ipEndpoint).then(i => i.json()).then(async ipJson => {
         const ip = config.ipKey.split(".").reduce((acc, curr) => acc?.[curr], ipJson);
         if (!ip || lastIp === ip) return;
         
-        console.log(timestamp(), `IP changed from '${lastIp || "nothing"}' to '${ip || "nothing"}'`);
+        console.log(timestamp(), `IP changed from '${lastIp || "nothing"}' to '${ip}'`);
 
-        await fetch(`https://api.cloudflare.com/client/v4/zones/${config.zoneId}/dns_records/${config.dnsRecordId}`, {
+        // Update IP if changed
+        const cfRes = await fetch(`https://api.cloudflare.com/client/v4/zones/${config.zoneId}/dns_records/${config.dnsRecordId}`, {
             method: "PATCH",
             headers: {
                 "Content-Type": "application/json",
@@ -19,18 +21,19 @@ let lastIp;
                 type: "A",
                 content: ip
             })
-        }).then(async res => {
-            if (res.status === 200) {
-                lastIp = ip;
-                return console.log(timestamp(), "Succesfully updated DNS!");
-            }
+        }).catch(err => console.log(timestamp(), "Failed to update DNS!"));
 
-            const json = await res.json().catch(err => { });
-            return console.log(timestamp(), `Failed to update DNS! Status: ${res.status}, JSON:`, json);
-        }).catch(err => console.log(timestamp(), "Failed to update DNS! Error:", err));
-    }).catch(err => console.log(timestamp(), "Failed to get IP! Error:", err));
+        if (cfRes.status !== 200) {
+            // Failed to update IP
+            return console.log(timestamp(), `Failed to update DNS! Status: ${res.status}`);
+        }
 
-    setTimeout(() => check(), config.checkInterval);
+        // Success
+        lastIp = ip;
+        return console.log(timestamp(), "Succesfully updated DNS!");
+    }).catch(err => console.log(timestamp(), "Failed to get IP!"));
+
+    setTimeout(() => check(), config.checkInterval); // Check IP loop
 })();
 
 function timestamp() {
